f9928cf6d149a2ee35e0054d575aa4763f4b354b
lucasandrade1981 <lucasandrade.foto@gmail.com>
2022-02-06 04:44:31 -0300

    Atualizaões - 06/02/2022

--- cursoUdemyJavaScript/Funções/Aula_01/scripts.js
+++ cursoUdemyJavaScript/Funções/Aula_01/scripts.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //escrevendo no console
 function escreverNoConsole(){
     console.log("Escrevendo no console.");
@@ -18,25 +17,4 @@ const textoPorParametro = function(texto){
     console.log(texto);
 }
 
-=======
-//escrevendo no console
-function escreverNoConsole(){
-    console.log("Escrevendo no console.");
-}
-
-escreverNoConsole();
-
-//texto no console
-const textoNoConsole = function(){
-    console.log("Texto no console.")
-}
-
-textoNoConsole();
-
-//texto por parametro   
-const textoPorParametro = function(texto){
-    console.log(texto);
-}
-
->>>>>>> b953e655741109c49765713791e56abe8f86bd45
 textoPorParametro("Texto por parâmetro.")
